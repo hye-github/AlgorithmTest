97b0d291516b2369a9514a5f91c79c0a132b5fa5
DESKTOP-LN9PLHV\zooc <hezuc12@gmail.com>
2023-02-13 23:49:35 +0900

    feat: add StrToInt

---
+++ Programmers/src/programmers/level1/stringtonum/Solution.java
@@ -0,0 +1,14 @@
+package programmers.level1.stringtonum;
+
+public class Solution {
+    public int solution(String s) {
+        return Integer.parseInt(s);
+    }
+
+    public static void main(String[] args) {
+        Solution sl = new Solution();
+        String s = "-1234";
+
+        System.out.println(sl.solution(s));
+    }
+}
