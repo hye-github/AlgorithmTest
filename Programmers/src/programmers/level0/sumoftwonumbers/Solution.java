1f85649596f27400c032abf171cd4660ea57a8fd
DESKTOP-LN9PLHV\zooc <hezuc12@gmail.com>
2023-03-06 23:41:34 +0900

    add: Sum of two numbers

---
+++ Programmers/src/programmers/level0/sumoftwonumbers/Solution.java
@@ -0,0 +1,14 @@
+package programmers.level0.sumoftwonumbers;
+
+public class Solution {
+    public int solution(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int num1 = 2;
+        int num2 = 3;
+        System.out.println(sol.solution(num1, num2));
+    }
+}
