a50d03f48c1a14dbb68b1c801a0ae80cfa905294
DESKTOP-LN9PLHV\zooc <hezuc12@gmail.com>
2023-03-08 13:43:12 +0900

    0레벨 3일차 2문제, 4일차 2문제 풀기

---
+++ Programmers/src/programmers/level0/sharingpizza3/Solution.java
@@ -0,0 +1,12 @@
+package programmers.level0.sharingpizza3;
+
+public class Solution {
+    public int solution(int n) {
+        return n%7==0 ? n/7 : n/7 + 1;
+    }
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int n = 1;
+        System.out.println(sol.solution(n));
+    }
+}
