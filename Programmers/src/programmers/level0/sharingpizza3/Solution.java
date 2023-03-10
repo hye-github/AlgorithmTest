7cbb1f507ee974eb2ed32ac640a1d002abc23f8d
DESKTOP-LN9PLHV\zooc <hezuc12@gmail.com>
2023-03-10 23:40:36 +0900

    0레벨 4일차 피자 나눠 먹기 (3)

--- Programmers/src/programmers/level0/sharingpizza3/Solution.java
+++ Programmers/src/programmers/level0/sharingpizza3/Solution.java
@@ -1,12 +1,13 @@
 package programmers.level0.sharingpizza3;
 
 public class Solution {
-    public int solution(int n) {
-        return n%7==0 ? n/7 : n/7 + 1;
+    public int solution(int slice, int n) {
+        return n%slice==0 ? n/slice : n/slice + 1;
     }
     public static void main(String[] args) {
         Solution sol = new Solution();
-        int n = 1;
-        System.out.println(sol.solution(n));
+        int slice = 4;
+        int n = 12;
+        System.out.println(sol.solution(slice, n));
     }
 }
