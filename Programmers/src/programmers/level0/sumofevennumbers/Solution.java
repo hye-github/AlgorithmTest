28ef66a08fb6f6bd8670e65f09053e0b21c0c0a1
DESKTOP-LN9PLHV\zooc <hezuc12@gmail.com>
2023-03-17 23:05:25 +0900

    0레벨 짝수의 합

---
+++ Programmers/src/programmers/level0/sumofevennumbers/Solution.java
@@ -0,0 +1,16 @@
+package programmers.level0.sumofevennumbers;
+
+public class Solution {
+    public int solution(int n) {
+        int answer=0;
+        for(int i=1; i<=n ; i++){
+            if(i%2==0){answer+=i;}
+        }
+        return answer;
+    }
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int n = 10;
+        System.out.println(sol.solution(n));
+    }
+}
